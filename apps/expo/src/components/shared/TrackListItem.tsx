6a6bd4c0d27426e2623dcc2a8b7b9efb1ce39560
Sander <mail@sanderboer.nl>
2023-03-28 00:57:31 +0200

    memoize TrackListItem for better performance on large lists

--- apps/expo/src/components/shared/TrackListItem.tsx
+++ apps/expo/src/components/shared/TrackListItem.tsx
@@ -1,17 +1,21 @@
-import { FC } from "react";
+import { FC, memo } from "react";
 
 import { ListItem, ListItemProps } from "./ListItem";
 
-export const TrackListItem: FC<Props> = ({ track, ...props }) => {
-  return (
-    <ListItem
-      {...props}
-      title={track.name}
-      subtitle={track.artists.map((artist) => artist.name).join(", ")}
-      imageUri={track.album.images[0]?.url}
-    />
-  );
-};
+export const TrackListItem: FC<Props> = memo(
+  ({ track, ...props }) => {
+    return (
+      <ListItem
+        {...props}
+        title={track.name}
+        subtitle={track.artists.map((artist) => artist.name).join(", ")}
+        imageUri={track.album.images[0]?.url}
+      />
+    );
+  },
+  (prev, next) =>
+    prev.track.id === next.track.id && prev.selected === next.selected,
+);
 
 interface Props extends Omit<ListItemProps, "title" | "subtitle" | "imageUri"> {
   track: SpotifyApi.TrackObjectFull;
