671b1f0877bf81b78e5e39c56a7a621b3739db17
Sander <mail@sanderboer.nl>
2024-05-10 10:28:30 +0200

    next 13.3.1 -> 14.2.3

--- apps/nextjs/src/providers/ThemeProvider.tsx
+++ apps/nextjs/src/providers/ThemeProvider.tsx
@@ -1,17 +1,9 @@
-import { createContext, useContext, useEffect, type FC, type PropsWithChildren } from "react";
 import { theme } from "@fissa/tailwind-config";
+import { createContext, useContext, useEffect, type FC, type PropsWithChildren } from "react";
 
 const ThemeContext = createContext({ theme });
 
 export const ThemeProvider: FC<PropsWithChildren> = ({ children }) => {
-  // const [selectedTheme, setSelectedTheme] = useLocalStorage("theme", theme);
-
-  // console.log(theme, selectedTheme);
-  // useEffect(() => {
-
-  // }, [selectedTheme])
-  // const theme = useRef
-
   // TODO: make this neater
   useEffect(() => {
     document.getElementsByTagName("main")[0]?.style.setProperty("background-color", theme["900"]);
